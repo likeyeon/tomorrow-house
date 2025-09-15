fb8b49b055232be6b2aa2b6abb207b469e07c806
likeyeon <chy98csy00@naver.com>
2025-09-15 17:29:28 +0900

    ♻️ Simplify carousel logic using spread operator

--- js/carousels.js
+++ js/carousels.js
@@ -1,41 +1,37 @@
-const productCarousel = tns({
-  container: '.product-carousel .slider-list',
-  controls: false,
-  nav: true,
-  navContainer: '.product-carousel .thumbnail-list',
+const commonOptions = {
   navAsThumbnails: true,
   arrowKeys: true,
-  autoplay: true,
-  items: 1,
-  autoplayHoverPause: true,
-  autoplayButtonOutput: false,
   mouseDrag: true,
   preventScrollOnTouch: true,
+}
+
+const productCarousel = tns({
+  ...commonOptions,
+  container: '.product-carousel .slider-list',
+  controls: false,
+  navContainer: '.product-carousel .thumbnail-list',
+  autoplay: true,
+  autoplayHoverPause: true,
+  autoplayButtonOutput: false,
 })
 
 const userGalleryMobile = tns({
+  ...commonOptions,
   container: '.user-gallery.is-mobile .slider-list',
   gutter: 4,
   edgePadding: 16,
-  loop: false,
   controls: false,
   navContainer: '.user-gallery.is-mobile .thumbnail-list',
-  navAsThumbnails: true,
-  arrowKeys: true,
-  mouseDrag: true,
-  preventScrollOnTouch: true,
+  loop: false,
 })
 
 const userGalleryDesktop = tns({
+  ...commonOptions,
   container: '.user-gallery.is-desktop .slider-list',
   gutter: 6,
   edgePadding: 75,
-  loop: false,
   controls: true,
   controlsContainer: '.user-gallery.is-desktop .user-gallery-controls',
   navContainer: '.user-gallery.is-desktop .thumbnail-list',
-  navAsThumbnails: true,
-  arrowKeys: true,
-  mouseDrag: true,
-  preventScrollOnTouch: true,
+  loop: false,
 })
